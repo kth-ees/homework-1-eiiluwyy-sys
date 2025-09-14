0d687164005294d95c56242be66851e9908f4dd2
eiiluwyy-sys <eiiluwyy@gmail.com>
2025-09-14 18:09:42 +0200

    111

--- q1/decoder.sv
+++ q1/decoder.sv
@@ -4,7 +4,17 @@ module decoder (
   output logic [15:0] one_hot
 );
 
-  // description here
+  // description here 111
 
+
+endmodule
+
+module decoder (
+  input logic [3:0] binary,
+
+  output logic [15:0] one_hot
+);
+
+  // description here 111
 
 endmodule
