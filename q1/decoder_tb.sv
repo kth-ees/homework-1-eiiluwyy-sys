2f40c01b87a089dfc851d625577aac23e7bc77ab
eiiluwyy-sys <eiiluwyy@gmail.com>
2025-09-14 22:23:17 +0200

    q1 finished

--- q1/decoder_tb.sv
+++ q1/decoder_tb.sv
@@ -9,6 +9,26 @@ module decoder_tb;
     .binary(binary),
     .one_hot(one_hot)
   );
+initial begin
 
-  // Complete your testbench here
+  $timeformat(-9, 0, "ns", 6);   // time format(nano_second£¬0£¬"ns"£¬min_widith=6)
+  $monitor("Time = %t, Binary = %4b, One-Hot = %16b",
+             $time, binary, one_hot);  //display every case
+  
+  binary = 4'b0000;             // initial variable
+  $display("Starting the testbench");
+
+    for (int i = 0 ; i<16 ; i++) begin
+     binary=i;
+  
+     #10;
+     end                  // try all posssible numbers
+
+
+  $display("Testbench completed.");
+  $finish;
+end
+
+
+  
 endmodule
